7715e0f94d69cc9a9f5192fee22f1b7d87005bd6
antihackss <vladmakov0859@gmail.com>
2025-06-08 14:54:53 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,65 +15,28 @@ void printPermutation(const std::vector<char>& perm) {
     std::cout << std::endl;
 }
 
-void runExperiment() {
-    std::vector<int> sizes = {3, 4, 5, 6, 7, 8};
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    
-    std::cout << "n\tgetAllPerms (ms)\tgetPerm1 (ms)\tgetPerm2 (ms)\n";
-    std::cout << "------------------------------------------------\n";
-    
-    for (int n : sizes) {
-        std::vector<char> elements;
-        for (int i = 1; i <= n; i++) {
-            elements.push_back('0' + i);
-        }
-        
-        PMTree tree(elements);
-        
-        std::uniform_int_distribution<> dist(1, tree.getAllPermsHelper().size());
-        int num = dist(gen);
-        
-        auto start = std::chrono::high_resolution_clock::now();
-        auto allPerms = ::getAllPerms(tree);
-        auto end = std::chrono::high_resolution_clock::now();
-        auto allPermsTime = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-        
-        start = std::chrono::high_resolution_clock::now();
-        auto perm1 = ::getPerm1(tree, num);
-        end = std::chrono::high_resolution_clock::now();
-        auto perm1Time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-        
-        start = std::chrono::high_resolution_clock::now();
-        auto perm2 = ::getPerm2(tree, num);
-        end = std::chrono::high_resolution_clock::now();
-        auto perm2Time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-        
-        std::cout << n << "\t"
-                  << allPermsTime << "\t\t\t"
-                  << perm1Time << "\t\t"
-                  << perm2Time << "\n";
-    }
-}
-
 int main() {
     std::vector<char> elements = {'1', '2', '3'};
     PMTree tree(elements);
-    
-    std::cout << "All permutations:\n";
-    auto allPerms = ::getAllPerms(tree);
+
+    std::cout << "All permutations:" << std::endl;
+    auto allPerms = getAllPerms(tree);
     for (const auto& perm : allPerms) {
         printPermutation(perm);
     }
-    
-    std::cout << "\nPermutation 1: ";
-    printPermutation(::getPerm1(tree, 1));
-    
+
+    std::cout << "\nTest cases:" << std::endl;
+    std::cout << "Permutation 1: ";
+    printPermutation(getPerm1(tree, 1));
+
     std::cout << "Permutation 2: ";
-    printPermutation(::getPerm2(tree, 2));
-    
-    std::cout << "\nRunning performance experiment...\n\n";
-    runExperiment();
-    
+    printPermutation(getPerm2(tree, 2));
+
+    std::cout << "Permutation 6: ";
+    printPermutation(getPerm1(tree, 6));
+
+    std::cout << "Permutation 8: ";
+    printPermutation(getPerm2(tree, 8));
+
     return 0;
 }
