d81aa737d9072a609b3a6c625ec20a9c6100d9db
antihackss <vladmakov0859@gmail.com>
2025-06-08 14:49:16 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -4,32 +4,35 @@
 
 #include <vector>
 #include <memory>
+#include <string>
 
 class PMTree {
 public:
-    explicit PMTree(const std::vector<char>& in);
-    int getTotalPermutations() const { return totalPermutations; }
+    explicit PMTree(const std::vector<char>& elements);
+    
     std::vector<std::vector<char>> getAllPerms() const;
     std::vector<char> getPermByNum(int num) const;
+    
+    std::vector<std::vector<char>> getAllPermsHelper() const;
+    std::vector<char> getPermByNumHelper(int num) const;
 
 private:
     struct Node {
         char value;
         std::vector<std::shared_ptr<Node>> children;
-        Node(char v) : value(v) {}
+        Node(char val) : value(val) {}
     };
-
+    
     std::shared_ptr<Node> root;
-    int totalPermutations;
-    std::vector<char> elements;
-
-    void buildTree(std::shared_ptr<Node> parent, const std::vector<char>& remaining);
-    void collectPermutations(std::shared_ptr<Node> node, 
-                           std::vector<char>& current, 
-                           std::vector<std::vector<char>>& result) const;
-    bool findPermByNum(std::shared_ptr<Node> node, 
-                      int& remainingSteps, 
-                      std::vector<char>& result) const;
+    size_t totalPermutations;
+    
+    void buildTree(std::shared_ptr<Node> node, const std::vector<char>& remaining);
+    void getAllPerms(std::shared_ptr<Node> node, std::vector<char>& current, std::vector<std::vector<char>>& result) const;
+    void getPermByNum(std::shared_ptr<Node> node, int& remainingSteps, std::vector<char>& result) const;
 };
+
+std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+std::vector<char> getPerm1(const PMTree& tree, int num);
+std::vector<char> getPerm2(const PMTree& tree, int num);
 
 #endif  // INCLUDE_TREE_H_
